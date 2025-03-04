9498ab2c02087763ea0b570db9e872925ba8e9c4
Márton <marton.fejer@gmail.com>
2025-03-04 07:28:26 +0100

    filtering bad values, interpolating

--- preproc.py
+++ preproc.py
@@ -1,24 +1,52 @@
-
 import h5py
 import numpy as np
 import pandas as pd
 
 
-def load_gaze_data(h5_file, dataset='gaze'):
+def load_gaze_data(h5_file: h5py.Dataset, output='pandas'):
     """
     Load gaze data from a Titta-generated HDF5 file.
     """
     with h5py.File(h5_file, 'r') as f:
-        data = pd.DataFrame(np.array(f[dataset]))
-    return data
+        labels = np.array(f['gaze']['axis0'])
 
-def degrees_to_pixels(degrees, screen_resolution, screen_width_cm, viewing_distance_cm):
+        categorical_data  = np.array(f['gaze']['block0_values'])
+        categorical_labels = labels[28:]
+
+        measurement_data = np.array(f['gaze']['block1_values'])
+        measurement_labels = labels[2:28]
+
+        timestamps = np.array(f['gaze']['block2_values'])
+        timestamp_labels = labels[:2]
+
+    if output == 'pandas':
+        categorical_data = pd.DataFrame(categorical_data, columns=categorical_labels)
+        measurement_data = pd.DataFrame(measurement_data, columns=measurement_labels)
+        timestamps = pd.DataFrame(timestamps, columns=timestamp_labels)
+
+        return categorical_data, measurement_data, timestamps
+
+    return (categorical_data, categorical_labels), (measurement_data, measurement_labels), (timestamps, timestamp_labels)
+
+def preprocess_raw(df: pd.DatFrame, filter: bool=False):
     """
-    Convert visual degrees to pixels.
+    Preprocess raw gaze data.
     """
-    pixels_per_cm = screen_resolution[0] / screen_width_cm
-    cm_per_degree = 2 * viewing_distance_cm * np.tan(np.deg2rad(degrees / 2))
-    return cm_per_degree * pixels_per_cm
+    # there might also be some bad values, out of range, filter those by setting to NaN, then interpolate
+    if filter:
+        for col in df.columns:
+            if col.startswith('right_gaze_point_on_display_area') or col.startswith('left_gaze_point_on_display_area'):
+                df[col] = df[col].apply(lambda x: x if 0 <= x <= 1 else np.nan)        
+
+    df['rx'] = df[b'right_gaze_point_on_display_area_x'].interpolate()
+    df['ry'] = df[b'right_gaze_point_on_display_area_y'].interpolate()
+    df['lx'] = df[b'left_gaze_point_on_display_area_x'].interpolate()
+    df['ly'] = df[b'left_gaze_point_on_display_area_y'].interpolate()
+
+    df['x'] = (df['rx'] + df['lx']) / 2
+    df['y'] = (df['ry'] + df['ly']) / 2
+
+    return df
 
 def calculate_dispersion(x, y):
     """
@@ -65,6 +93,7 @@ def dispersion_fixation_detection(gaze_data, dispersion_threshold, min_duration_
 
 
 if __name__ == '__main__':
-    gaze_data = load_gaze_data('data.h5')
-    fixations = dispersion_fixation_detection(gaze_data, dispersion_threshold=degrees_to_pixels(1, (1920, 1080), 53.13, 60), min_duration_ms=100)
-    print(fixations)
+    cat, con, time = load_gaze_data('test.h5')
+    df = pd.concat([con, time])
+    fixations = dispersion_fixation_detection(df, dispersion_threshold=1, min_duration_ms=100)
+    # print(fixations)
